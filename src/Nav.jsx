10c1f40c0e3bd1b79756dd3d0a0a89cc908932b5
BinayakBishnu <bishnu.binayak12@gmail.com>
2022-06-19 12:34:16 +0530

    feat: navbar completed, header tag edited for logo

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -8,6 +8,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faBehance } from '@fortawesome/free-brands-svg-icons'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faContactCard } from '@fortawesome/free-regular-svg-icons'
 
 import navStyles from './Navbar.module.css'
@@ -38,34 +39,34 @@ function Nav() {
                     <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
                         <ReactBootstrap.Nav className="me-auto">
-                            <ReactBootstrap.Nav.Link href="#aboutme" className={`${navStyles.links} ${navStyles.leftLinks}`}>About Me</ReactBootstrap.Nav.Link>
-                            <ReactBootstrap.Nav.Link href="#techStack" className={`${navStyles.links} ${navStyles.leftLinks}`}>Tech Stack</ReactBootstrap.Nav.Link>
-                            <ReactBootstrap.NavDropdown title="Top Projects" id="collasible-nav-dropdown" className={`${navStyles.links} ${navStyles.leftLinks}`}>
-                                <ReactBootstrap.NavDropdown.Item href="#action/3.1">Project 1</ReactBootstrap.NavDropdown.Item>
-                                <ReactBootstrap.NavDropdown.Item href="#action/3.2">Project 2</ReactBootstrap.NavDropdown.Item>
-                                <ReactBootstrap.NavDropdown.Item href="#action/3.3">Project 3</ReactBootstrap.NavDropdown.Item>
+                            <ReactBootstrap.Nav.Link href="#home" className={`${navStyles.links} ${navStyles.leftLinks}`}>Home</ReactBootstrap.Nav.Link>
+                            <ReactBootstrap.Nav.Link href="#consult" className={`${navStyles.links} ${navStyles.leftLinks}`}>Consult</ReactBootstrap.Nav.Link>
+                            <ReactBootstrap.Nav.Link href="#order" className={`${navStyles.links} ${navStyles.leftLinks}`}>Order</ReactBootstrap.Nav.Link>
+                            <ReactBootstrap.NavDropdown title="Profile" id="collasible-nav-dropdown" className={`${navStyles.links} ${navStyles.leftLinks}`}>
+                                <ReactBootstrap.NavDropdown.Item href="#editProfile">Edit profile</ReactBootstrap.NavDropdown.Item>
+                                <ReactBootstrap.NavDropdown.Item href="#paymentHistory">See payment history</ReactBootstrap.NavDropdown.Item>
                                 <ReactBootstrap.NavDropdown.Divider />
-                                <ReactBootstrap.NavDropdown.Item href="#action/3.4">Other Projects</ReactBootstrap.NavDropdown.Item>
+                                <ReactBootstrap.NavDropdown.Item href="#logout">Log Out</ReactBootstrap.NavDropdown.Item>
                             </ReactBootstrap.NavDropdown>
                         </ReactBootstrap.Nav>
                         <ReactBootstrap.Nav className={`${navStyles.socialLinksParent} d-flex flex-row`}>
-                            <ReactBootstrap.Nav.Link href="https://www.github.com/BinayakBishnu"
-                                target="_blank"
-                                className={`${navStyles.socialLinks} ${navStyles.links}`}
-                            >
-                                <FontAwesomeIcon icon={faGithub} />
-                            </ReactBootstrap.Nav.Link>
                             <ReactBootstrap.Nav.Link href="https://www.linkedin.com/in/binayakbishnu"
                                 target="_blank"
                                 className={`${navStyles.socialLinks} ${navStyles.links} ms-2 ms-md-1`}
                             >
                                 <FontAwesomeIcon icon={faLinkedin} />
                             </ReactBootstrap.Nav.Link>
-                            <ReactBootstrap.Nav.Link href="https://www.behance.net/binayakbishnu"
+                            <ReactBootstrap.Nav.Link href="https://twitter.com/binayak_bishnu"
                                 target="_blank"
                                 className={`${navStyles.socialLinks} ${navStyles.links} ms-2 ms-md-1`}
                             >
-                                <FontAwesomeIcon icon={faBehance} />
+                                <FontAwesomeIcon icon={faTwitter} />
+                            </ReactBootstrap.Nav.Link>
+                            <ReactBootstrap.Nav.Link href="https://www.github.com/BinayakBishnu"
+                                target="_blank"
+                                className={`${navStyles.socialLinks} ${navStyles.links}`}
+                            >
+                                <FontAwesomeIcon icon={faGithub} />
                             </ReactBootstrap.Nav.Link>
                             <ReactBootstrap.Nav.Link href="https://www.instagram.com/binayak_bishnu"
                                 target="_blank"
