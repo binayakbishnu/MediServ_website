698ee7557c84f4820a7ad63ec2d9906e9ab495a6
BinayakBishnu <bishnu.binayak12@gmail.com>
2022-06-27 14:04:57 +0530

    feat: confpass and functioning in profile form

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -9,17 +9,27 @@ export class ProfilePage extends Component {
         super(props);
         this.state = {
             id: uuid(),
-            firstName: 'Binayak',
-            lastName: 'Bishnu',
-            email: 'bishnu.binayak12@gmail.com',
-            password: 'qweewq123',
+            currentFirstName: 'Binayak',
+            currentLastName: 'Bishnu',
+            currentEmail: 'bishnu.binayak12@gmail.com',
+            currentPassword: 'qweewq123',
+            oldFirstName: 'Binayak',
+            oldLastName: 'Bishnu',
+            oldEmail: 'bishnu.binayak12@gmail.com',
+            oldPassword: 'qweewq123',
+            confirmPassword: '',
+
+            firstNameError: '',
+            lastNameError: '',
+            emailError: '',
+            passwordError: '',
+            confirmPasswordError: '',
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(event) {
-        // alert(event.target.value);
         const name = event.target.name;
         var value;
         if (event.target.type === 'checkbox') {
@@ -56,7 +66,7 @@ export class ProfilePage extends Component {
         const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if (email === "" || email === null) {
             // alert("Enter email");
-            this.setState({ email: '' });
+            this.setState({ currentEmail: '' });
             this.setState({ emailError: 'Enter email' });
             return false;
         } else if (email.match(regexEmail)) {
@@ -65,7 +75,7 @@ export class ProfilePage extends Component {
             return true;
         } else {
             // alert("Invalid email address");
-            this.setState({ email: '' });
+            this.setState({ currentEmail: '' });
             this.setState({ emailError: 'Invalid email' });
             return false;
         }
@@ -74,13 +84,13 @@ export class ProfilePage extends Component {
     passwordValidation(password) {
         if (password === "" || password === null) {
             // alert("Enter password");
-            this.setState({ password: '' });
+            this.setState({ currentPassword: '' });
             this.setState({ confirmPassword: '' });
             this.setState({ passwordError: 'Enter password' });
             return false;
         } else if (password.length <= 5) {
             // alert("Password too short");
-            this.setState({ password: '' });
+            this.setState({ currentPassword: '' });
             this.setState({ confirmPassword: '' });
             this.setState({ passwordError: 'Password too short' });
             return false;
@@ -93,12 +103,12 @@ export class ProfilePage extends Component {
 
     confirmPasswordValidation(password, confirmPassword) {
         if (confirmPassword === "" || confirmPassword === null) {
-            this.setState({ password: '' });
+            this.setState({ currentPassword: '' });
             this.setState({ confirmPassword: '' });
             this.setState({ confirmPasswordError: 'Enter password' });
             return false;
         } else if (confirmPassword !== password) {
-            this.setState({ password: '' });
+            this.setState({ currentPassword: '' });
             this.setState({ confirmPassword: '' });
             this.setState({ confirmPasswordError: "Passwords don't match" });
             return false;
@@ -109,16 +119,17 @@ export class ProfilePage extends Component {
     }
 
     handleSubmit(event) {
-        alert(`Submitted: ${this.state.id}, ${this.state.id.length},\n${this.state.firstName} ${this.state.lastName}, ${this.state.email}, ${this.state.password}, ${this.state.confirmPassword}`);
+        alert(event.target.name);
+        alert(`Submitted: ${this.state.id}, ${this.state.id.length},\n${this.state.currentFirstName} ${this.state.currentLastName}, ${this.state.currentEmail}, ${this.state.currentPassword}, ${this.state.confirmPassword}`);
 
         event.preventDefault();
-        const { firstName, lastName, email, password, confirmPassword } = this.state
+        const { currentFirstName, currentLastName, currentEmail, currentPassword, confirmPassword } = this.state
 
-        const firstNameValidation = this.nameValidation('firstName', firstName);
-        const lastNameValidation = this.nameValidation('lastName', lastName);
-        const emailValidation = this.emailValidation(email);
-        const passwordValidation = this.passwordValidation(password);
-        const confirmPasswordValidation = this.confirmPasswordValidation(password, confirmPassword);
+        const firstNameValidation = this.nameValidation('currentFirstName', currentFirstName);
+        const lastNameValidation = this.nameValidation('currentLastName', currentLastName);
+        const emailValidation = this.emailValidation(currentEmail);
+        const passwordValidation = this.passwordValidation(currentPassword);
+        const confirmPasswordValidation = this.confirmPasswordValidation(currentPassword, confirmPassword);
         alert(`${firstNameValidation}, ${lastNameValidation}\n${emailValidation}, ${passwordValidation}, ${confirmPasswordValidation}`);
 
         // const url = '/mainApp'
@@ -134,29 +145,87 @@ export class ProfilePage extends Component {
         this.setState({
             id: uuid(),
         })
+
+        return firstNameValidation && lastNameValidation && emailValidation && passwordValidation && confirmPasswordValidation;
     }
 
-    editProfile() {
+    editProfile(name) {
+        this.setState({
+            oldFirstName: this.state.currentFirstName,
+            oldLastName: this.state.currentLastName,
+            oldEmail: this.state.currentEmail,
+            oldPassword: this.state.currentPassword,
+        })
+
+        alert(name + ' clicked');
+
+        var purpose;
+        if (name === "editProfile") {
+            if (document.getElementById("saveChangesCol").style.visibility === "hidden") {
+                purpose = "editing";
+            } else {
+                purpose = "cancelling"
+            }
+        } else if (name === "saveChanges") {
+            purpose = "saving";
+        }
+
+        if (purpose === "saving") {
+            // var formValidation = this.handleSubmit(document.getElementById('profileForm'));
+            var formValidation = this.state.confirmPassword === '' || this.state.confirmPassword === null ? false : this.state.confirmPassword === this.state.currentPassword ? true : false;
+            if (formValidation === true) {
+                alert("Form is ok");
+            } else {
+                alert("Form is not ok");
+                this.setState({ confirmPasswordError: "Invalid" })
+                return false;
+            }
+        }
+
         document.getElementById('saveChangesCol').style.visibility =
             document.getElementById('saveChangesCol').style.visibility === 'visible' ? 'hidden' : 'visible';
         document.getElementById('editProfile').innerHTML =
             document.getElementById('editProfile').innerHTML === "Edit Profile" ? "Cancel" : "Edit Profile";
-        document.getElementById('firstName').disabled =
-            document.getElementById('firstName').disabled === true ? false : true;
-        document.getElementById('lastName').disabled =
-            document.getElementById('lastName').disabled === true ? false : true;
-        document.getElementById('email').disabled =
-            document.getElementById('email').disabled === true ? false : true;
-        document.getElementById('password').disabled =
-            document.getElementById('password').disabled === true ? false : true;
+        document.getElementById('currentFirstName').disabled =
+            document.getElementById('currentFirstName').disabled === true ? false : true;
+        document.getElementById('currentLastName').disabled =
+            document.getElementById('currentLastName').disabled === true ? false : true;
+        document.getElementById('currentEmail').disabled =
+            document.getElementById('currentEmail').disabled === true ? false : true;
+        document.getElementById('currentPassword').disabled =
+            document.getElementById('currentPassword').disabled === true ? false : true;
+        document.getElementById('confirmPasswordRow').style.display =
+            document.getElementById('confirmPasswordRow').style.display === 'none' ? 'block' : 'none';
+
+        if (purpose === "cancelling") {
+            alert("Cancelling")
+            this.setState({
+                currentFirstName: this.state.oldFirstName,
+                currentLastName: this.state.oldLastName,
+                currentEmail: this.state.oldEmail,
+                currentPassword: this.state.oldPassword,
+            })
+        } else if (purpose === "saving") {
+            alert("Saving");
+            this.setState({
+                oldFirstName: this.state.currentFirstName,
+                oldLastName: this.state.currentLastName,
+                oldEmail: this.state.currentEmail,
+                oldPassword: this.state.currentPassword,
+            })
+        } else if (purpose === "editing") {
+            alert("Editing")
+        }
+
+        alert(`Old: ${this.state.oldFirstName}, ${this.state.oldLastName}, ${this.state.oldEmail}, ${this.state.oldPassword}\nNew: ${this.state.currentFirstName}, ${this.state.currentLastName}, ${this.state.currentEmail}, ${this.state.currentPassword}`);
     }
 
     render() {
         return (
             <div className={`${profilePageStyles.profileParent}`}>
-                <Form action='/mainApp' method='POST' onSubmit={this.handleSubmit} id="form">
+                <Form name="profileForm" action='/mainApp' method='POST' onSubmit={this.handleSubmit} id="profileForm">
                     <h3>Profile Page</h3>
-                    <Row className="mb-5">
+                    <Row name="idButtonsRow" className="mb-5">
                         <Col className="" md="6">
                             <label>ID</label>
                             <Form.Control
@@ -170,62 +239,78 @@ export class ProfilePage extends Component {
                         </Col>
                         <Col md="2" className="">
                             <label style={{ "visibility": "hidden" }}>Edit Profile</label>
-                            <Button type="button" className="btn btn-primary" onClick={this.editProfile}
-                                id="editProfile">
+                            <Button type="button" className="btn btn-primary" onClick={e => this.editProfile(e.target.name)}
+                                id="editProfile" name="editProfile">
                                 Edit Profile
                             </Button>
                         </Col>
                         <Col md="2" className="" style={{ "visibility": "hidden" }} id="saveChangesCol">
                             <label style={{ "visibility": "hidden" }}>Save changes</label>
-                            <Button type="button" className="btn btn-primary" onClick={this.editProfile}>
+                            <Button type="button" className="btn btn-primary" onClick={e => this.editProfile(e.target.name)}
+                                id="saveChanges" name="saveChanges">
                                 Save changes
                             </Button>
                         </Col>
                     </Row>
 
-                    <Row className="mb-5">
+                    <Row name="namesRow" className="mb-5">
                         <Col className="" md="6">
                             <label>First Name</label>
-                            <Form.Control name="firstName"
-                                value={this.state.firstName} onChange={this.handleChange}
+                            <Form.Control name="currentFirstName"
+                                value={this.state.currentFirstName} onChange={this.handleChange}
                                 className="form-control"
-                                id="firstName"
+                                id="currentFirstName"
                                 disabled
                             />
-                            <span name="firstNameError" className="">{this.state.doctorError}</span>
+                            <span name="firstNameError" className="">{this.state.firstNameError}</span>
                         </Col>
                         <Col className="" md="6">
                             <label>Last Name</label>
-                            <Form.Control name="lastName"
-                                value={this.state.lastName} onChange={this.handleChange}
+                            <Form.Control name="currentLastName"
+                                value={this.state.currentLastName} onChange={this.handleChange}
                                 className="form-control"
-                                id="lastName"
+                                id="currentLastName"
                                 disabled
                             />
-                            <span name="lastNameError" className="">{this.state.doctorError}</span>
+                            <span name="lastNameError" className="">{this.state.lastNameError}</span>
                         </Col>
                     </Row>
 
-                    <Row className="mb-0">
+                    <Row name="emailPasswordRow" className="mb-0" id="emailPasswordRow">
                         <Col className="" md="6">
                             <label>Email</label>
-                            <Form.Control name="email"
-                                value={this.state.email} onChange={this.handleChange}
+                            <Form.Control name="currentEmail" type="email"
+                                value={this.state.currentEmail} onChange={this.handleChange}
                                 className="form-control"
-                                id="email"
+                                id="currentEmail"
                                 disabled
                             />
-                            <span name="emailError" className="">{this.state.doctorError}</span>
+                            <span name="emailError" className="">{this.state.emailError}</span>
                         </Col>
                         <Col className="" md="6">
-                            <label>Password</label>
-                            <Form.Control name="password" type="password"
-                                value={this.state.password} onChange={this.handleChange}
-                                className="form-control"
-                                id="password"
-                                disabled
-                            />
-                            <span name="passwordError" className="">{this.state.doctorError}</span>
+                            <Row>
+                                <Col>
+                                    <label>Password</label>
+                                    <Form.Control name="currentPassword" type="password"
+                                        value={this.state.currentPassword} onChange={this.handleChange}
+                                        className="form-control"
+                                        id="currentPassword"
+                                        disabled
+                                    />
+                                    <span name="passwordError" className="">{this.state.passwordError}</span>
+                                </Col>
+                            </Row>
+                            <Row name="confirmPasswordRow" id="confirmPasswordRow" className="mt-2" style={{ "display": "none" }}>
+                                <Col>
+                                    <label>Confirm Password<sup>*</sup></label>
+                                    <Form.Control name="confirmPassword" type="password"
+                                        value={this.state.confirmPassword} onChange={this.handleChange}
+                                        className="form-control"
+                                        id="confirmPassword"
+                                    />
+                                    <span name="confirmPasswordError" className="">{this.state.confirmPasswordError}</span>
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
 
